2c80470b6372874bb9b1aa410265c9710d3ab221
dlevkovits <dlevkovits@gmail.com>
2016-04-19 14:43:12 -0400

    Instagram feed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $('.lines').click(function(){
 	$('.hamburger-items').toggleClass('close');
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+  });
+});
